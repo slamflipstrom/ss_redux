fac5ed5d28c6d68a574e0f14c8e7adff1ae40cbf
Dave Kiss <dave@davekiss.com>
2017-05-20 08:49:00 -0600

    Replace playerCount property
    Each reducer is passed only its own state property, NOT the entire state.
    
    So, you don't need to adjust a nested property here (since it isn't actually nested)

--- src/reducers/game.js
+++ src/reducers/game.js
@@ -5,7 +5,7 @@ const defaultState = {
 export default (state = defaultState, action) => {
   switch (action.type) {
     case "ADD_PLAYER":
-      return [...state, { playerCount: action.playerCount + 1 }];
+      return { ...state, playerCount: action.playerCount + 1 };
     case "REMOVE_PLAYER":
       return [...state, action.playerCount];
     default:
