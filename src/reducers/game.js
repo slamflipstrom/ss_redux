eb76173207829887742a87f0992992e235f57d9a
Dave Kiss <dave@davekiss.com>
2017-05-20 08:47:15 -0600

    Define default state shape in reducer

--- src/reducers/game.js
+++ src/reducers/game.js
@@ -1,4 +1,8 @@
-export default (state = 0, action) => {
+const defaultState = {
+	playerCount: 0,
+}
+
+export default (state = defaultState, action) => {
   switch (action.type) {
     case "ADD_PLAYER":
       return [...state, { playerCount: action.playerCount + 1 }];
