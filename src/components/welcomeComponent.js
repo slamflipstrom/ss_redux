934fe9aa5f7dfbd467e7e18db08b41985073dc90
Sam Lindstrom <lindstrom.s@gmail.com>
2017-06-19 08:12:38 -0600

    Throw error if incorrect playerCount

--- src/components/welcomeComponent.js
+++ src/components/welcomeComponent.js
@@ -3,8 +3,6 @@ import React, { Component } from "react";
 class WelcomeComponent extends Component {
   constructor(props) {
     super(props);
-
-    this.addPlayer = this.addPlayer.bind(this);
   }
 
   addPlayer(playerCount) {
@@ -22,6 +20,15 @@ class WelcomeComponent extends Component {
       this.props.removePlayer(playerCount);
     }
   }
+
+  startGame(playerCount) {
+    if (playerCount === 1 || playerCount === 5) {
+      throw "Somerset can only be played with 2, 3, 4, or 6 players";
+    } else {
+      this.props.startGame();
+    }
+  }
+
   render() {
     const PlayerCount = this.props.playerCount;
     return (
@@ -29,6 +36,8 @@ class WelcomeComponent extends Component {
         <p>Player Count: {PlayerCount}</p>
         <button onClick={() => this.addPlayer(PlayerCount)}>Add Player</button>
         <button onClick={() => this.removePlayer(PlayerCount)}>Remove Player</button>
+        <br />
+        <button onClick={() => this.startGame(PlayerCount)}>Start Game</button>
       </div>
     );
   }
